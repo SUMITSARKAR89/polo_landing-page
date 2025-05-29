fea7f823caf6db39471cc3b6eb386a07b8adfcd7
Sumit-sarkar <sumitopticalit89@gmail.com>
2025-05-29 21:48:34 +0600

    working index

--- asset/JsFile/app.js
+++ asset/JsFile/app.js
@@ -4,3 +4,22 @@ const cursor = document.getElementById("cursor");
       cursor.style.top = `${e.clientY}px`;
       cursor.style.left = `${e.clientX}px`;
     });
+
+
+
+// ==========project card=============
+
+
+const projectCard = document.querySelectorAll('.project-card');
+
+projectCard.forEach(card => {
+    const icon = card.querySelector('.project-icon');
+
+    card.addEventListener('mousemove', (e) => {
+      let rect = card.getBoundingClientRect();
+      let x = e.clientX - rect.left;
+      let y = e.clientY - rect.top;
+      icon.style.left = `${x}px`;
+      icon.style.top = `${y}px`;
+    });
+  });
